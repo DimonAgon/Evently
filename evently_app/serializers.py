35f3d48fb694c5ed7ca453c4ea549714398ce4b6
DimonAgon <dimon2003agon@gmail.com>
2024-12-19 21:52:10 +0200

    create primary api

--- evently_app/serializers.py
+++ evently_app/serializers.py
@@ -1,2 +1,28 @@
 
 from rest_framework import serializers
+
+from .models import *
+
+import copy
+
+from typing import Dict, Any
+
+
+class EventSerializer(serializers.ModelSerializer):
+    location = serializers.CharField()
+    organizer = serializers.CharField()
+    class Meta:
+        model = Event
+        fields = "__all__"
+
+    def create(self, validated_data) -> Event:
+        validated_data_copy = copy.deepcopy(validated_data)
+        location_name = validated_data['location']
+        del validated_data_copy['location']
+        location = Location.objects.get_or_create(name=location_name)[0]
+        del validated_data_copy['organizer']
+        organizer_name = validated_data['organizer']
+        organizer = Organizer.objects.get_or_create(name=organizer_name)[0]
+        new_event = Event(location=location, organizer=organizer, **validated_data_copy)
+        new_event.save()
+        return new_event
--- evently_app/views.py
+++ evently_app/views.py
@@ -1,2 +1,9 @@
 
 from rest_framework import viewsets
+
+from .serializers import *
+
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
