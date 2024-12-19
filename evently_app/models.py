7f16980e040b972d2d09de25e5414f10255b93f8
DimonAgon <dimon2003agon@gmail.com>
2024-12-19 20:53:01 +0200

    add models, make migrations

--- evently_app/models.py
+++ evently_app/models.py
@@ -1,3 +1,18 @@
+
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=900)
+    description = models.TextField()
+    datetime = models.DateTimeField()
+    location = models.ForeignKey('Location', on_delete=models.PROTECT)
+    organizer = models.ForeignKey('Organizer', on_delete=models.PROTECT)
+
+
+class Location(models.Model):
+    name = models.CharField(max_length=600)
+
+
+class Organizer(models.Model):
+    name = models.CharField(max_length=300)
