53056e0779f8372992257c701652aa5277485042
DimonAgon <dimon2003agon@gmail.com>
2025-02-20 17:10:07 +0200

    fix slug entering for event location and organizer

--- evently_app/serializers.py
+++ evently_app/serializers.py
@@ -3,28 +3,31 @@ from rest_framework import serializers
 
 from .models import *
 
-import copy
-
 from typing import Dict, Any
 
 
+class GetOrCreateSlugRelatedField(serializers.SlugRelatedField):
+    def to_internal_value(self, data: str):
+        return self.get_queryset().get_or_create(**{self.slug_field: data})[0]
+
 class EventSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(slug_field='username', read_only=True)
-    location = serializers.CharField()
-    organizer = serializers.CharField()
+    location = GetOrCreateSlugRelatedField(
+        slug_field='name',
+        queryset=Location.objects.all(),
+        style={'base_template': 'textarea.html'}
+    )
+    organizer = GetOrCreateSlugRelatedField(
+        slug_field='name',
+        queryset=Organizer.objects.all(),
+        style={'base_template': 'textarea.html'}
+    )
     class Meta:
         model = Event
         fields = "__all__"
 
     def create(self, validated_data: Dict[str, Any]) -> Event:
         user = self.context['request'].user
-        validated_data_copy = copy.deepcopy(validated_data)
-        location_name = validated_data['location']
-        del validated_data_copy['location']
-        location = Location.objects.get_or_create(name=location_name)[0]
-        del validated_data_copy['organizer']
-        organizer_name = validated_data['organizer']
-        organizer = Organizer.objects.get_or_create(name=organizer_name)[0]
-        new_event = Event(location=location, organizer=organizer, **validated_data_copy, user=user)
+        new_event = Event(**validated_data, user=user)
         new_event.save()
         return new_event
